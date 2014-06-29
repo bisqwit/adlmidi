bisqwit/adlmidi

commit 5395fe75f2053e0e062b2cd780bdef3f7bd3346b

Improve commandline parsing (and stdout outputting) when -w is used

midiplay.cc
@@ -2577,10 +2577,11 @@ static void SendStereoAudio(unsigned long count, int* samples)
     if(WritePCMfile)
     {
         /* HACK: Cheat on DOSBox recording: Record audio separately on Windows. */
-        static FILE* fp = 0;
+        static FILE* fp = nullptr;
         if(!fp)
         {
-            fp = fopen(PCMfilepath.c_str(), "wb");
+            fp = PCMfilepath == "-" ? stdout
+                                    : fopen(PCMfilepath.c_str(), "wb");
             if(fp)
             {
                 FourChars Bufs[] = {
@@ -2911,6 +2912,8 @@ int main(int argc, char** argv)
 
     while(argc > 2)
     {
+        bool had_option = false;
+
         if(!std::strcmp("-p", argv[2]))
             AdlPercussionMode = true;
         else if(!std::strcmp("-v", argv[2]))
@@ -2922,15 +2925,27 @@ int main(int argc, char** argv)
         else if(!std::strcmp("-w", argv[2]))
         {
             WritePCMfile = true;
-            if (argc > 3)
-                PCMfilepath = argv[3];
+            if (argc > 3 && argv[3][0] != '\0' && (argv[3][0] != '-' || argv[3][1] == '\0'))
+            {
+                // Allow the option argument if
+                // - it's not empty, and...
+                // - it does not begin with "-" or it is "-"
+                // - it is not a positive integer
+                char* endptr = 0;
+                if(std::strtol(argv[3], &endptr, 10) < 0 || (endptr && *endptr))
+                {
+                    PCMfilepath = argv[3];
+                    had_option  = true;
+                }
+            }
         }
         else if(!std::strcmp("-s", argv[2]))
             ScaleModulators = true;
         else break;
 
-        for(int p=2; p<argc; ++p) argv[p] = argv[p+1];
-        --argc;
+        std::copy(argv + (had_option ? 4 : 2), argv + argc,
+                  argv+2);
+        argc -= (had_option ? 2 : 1);
     }
 
 #ifndef __DJGPP__
@@ -2974,7 +2989,8 @@ int main(int argc, char** argv)
             std::fprintf(stderr, "bank number may only be 0..%u.\n", NumBanks-1);
             return 0;
         }
-        std::printf("FM instrument bank %u selected.\n", AdlBank);
+        if(WritingToTTY)
+            std::printf("FM instrument bank %u selected.\n", AdlBank);
     }
 
     unsigned n_fourop[2] = {0,0}, n_total[2] = {0,0};

gen_adldata.cc
@@ -376,7 +376,7 @@ static void LoadBNK(const char* fn, unsigned bank, const char* prefix, bool is_f
         const size_t offset2 = data_offset + data_index * 30;
         const unsigned char mode      = data[offset2+0];
         const unsigned char voice_num = data[offset2+1];
-        const unsigned char* op1 = &data[offset2+2];  // 13 bytes 
+        const unsigned char* op1 = &data[offset2+2];  // 13 bytes
         const unsigned char* op2 = &data[offset2+15];
         const unsigned char waveform_mod = data[offset2+28];
         const unsigned char waveform_car = data[offset2+29];
@@ -1294,7 +1294,7 @@ int main()
      "HMI (Descent 2)",        //d2melo,d2drum
      "HMI (Normality)",        //normmelo,normdrum
      "HMI (Shattered Steel)",  //ssmelo,ssdrum
-     "HMI (Theme Park)", // file131, file132 
+     "HMI (Theme Park)", // file131, file132
      // 10
      "HMI (3d Table Sports, Battle Arena Toshinden)", //file133, file134
      "HMI (Aces of the Deep)", //file142, file143
@@ -1362,7 +1362,7 @@ int main()
     for(unsigned a=0; a<36*8; ++a)
     {
         if( (1 << (a%8)) > maxvalues[a/8]) continue;
-        
+
         const std::map<unsigned,unsigned>& data = Correlate[a];
         if(data.empty()) continue;
         std::vector< std::pair<unsigned,unsigned> > correlations;

commit c6d3fa968b7a54f9cd159d7db29accd538c127b0

Fix commandline parsing bug introduced in commit 5395fe75f2053e0e062b2cd780bdef3f7bd3346b

midiplay.cc
@@ -2943,7 +2943,7 @@ int main(int argc, char** argv)
             ScaleModulators = true;
         else break;
 
-        std::copy(argv + (had_option ? 4 : 2), argv + argc,
+        std::copy(argv + (had_option ? 4 : 3), argv + argc,
                   argv+2);
         argc -= (had_option ? 2 : 1);
     }